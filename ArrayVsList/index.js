11960800603da4130865caa70dfa79491fa72dd7
bsihame <bsihame@hotmail.com>
2023-05-05 23:07:48 -0400

    solve Leetcode 136 Single Number

--- ArrayVsList/index.js
+++ ArrayVsList/index.js
@@ -344,3 +344,55 @@ console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 2]));
 
 // time complexity : O(n);
 // space complexity : 0(n);
+
+console.log("=======================================================");
+console.log("Single Number");
+/**leetcode 136. Single Number
+Easy
+13.7K
+526
+Companies
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once. */
+function singleNumber(array) {
+  let freqNum = {};
+  for (let i = 0; i < array.length; i++) {
+    if (freqNum[array[i]]) {
+      freqNum[array[i]] += 1;
+    } else {
+      freqNum[array[i]] = 1;
+    }
+  }
+  for (let key in freqNum) {
+    if (freqNum[key] < 2) {
+      return key;
+    }
+  }
+}
+console.log(singleNumber([2, 2, 1])); // 1
+console.log(singleNumber([1])); // 1
+console.log(singleNumber([1, 3, 1, 4, 4, 5, 5])); // 3
+console.log(singleNumber([4, 1, 2, 1, 2])); // 4
