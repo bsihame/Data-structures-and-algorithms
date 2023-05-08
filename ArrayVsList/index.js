bfe0ab389e91ac10e66071d0ce58398982185c90
bsihame <bsihame@hotmail.com>
2023-05-07 22:32:10 -0400

    create time table: 2D arraay

--- ArrayVsList/index.js
+++ ArrayVsList/index.js
@@ -396,3 +396,22 @@ console.log(singleNumber([2, 2, 1])); // 1
 console.log(singleNumber([1])); // 1
 console.log(singleNumber([1, 3, 1, 4, 4, 5, 5])); // 3
 console.log(singleNumber([4, 1, 2, 1, 2])); // 4
+
+console.log("=======================================================");
+console.log("2D array");
+
+let timeTable = [
+  [1, 2, 3, 4, 5],
+  [2, 4, 6, 8, 10],
+  [3, 6, 9, 12, 15],
+  [4, 8, 12, 16, 20],
+  [5, 10, 15, 20, 25],
+];
+for (let row = 0; row < timeTable.length; row++) {
+  let line = "";
+  for (let column = 0; column < timeTable[row].length; column++) {
+    let value = timeTable[row][column];
+    line += value + " ";
+  }
+  console.log(line);
+}
